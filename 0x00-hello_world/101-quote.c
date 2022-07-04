1fec67c1dc81556fc8a3d3f650e8722a684c377f
creati-vely <chivunaw@gmail.com>
2022-07-04 10:42:54 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  * followed by new line, to the standard erros.
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 write(2, "and that piece od art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
