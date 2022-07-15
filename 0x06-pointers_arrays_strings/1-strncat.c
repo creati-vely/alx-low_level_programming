2e91f29bf18bc64d8f8c06546f959fe5ce9016ef
creati-vely <chivunaw@gmail.com>
2022-07-15 10:51:41 -0700

    hot fix task #1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,14 +2,20 @@
 #include <string.h>
 
 /**
- * _strncat - function to append some charx
- * @dest: first param
- * @src: second param
- * @n: third param
- * Return: string
+ * _strncat - concatenate two strings but add inputted number of bytes
+ * @dest: string to be appended upon
+ * @src: string to be completed at the end of dest
+ * @n: integer parameter to compare index to
+ * Return: return new concatenated string
  */
 char *_strncat(char *dest, char *src, int n);
 {
-	strncat(*dest, *src, n);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
