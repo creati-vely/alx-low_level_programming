30778300a4518f8edca39411b3c7ee18221e9831
creati-vely <chivunaw@gmail.com>
2022-10-18 09:34:53 -0700

    prints _putchar, followed by a new line

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 01; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = 01; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 == 9 && num2 == 9)
+			if (num1 == 8 && num2 == 8)
 				continue;
 
 			putchar(',');
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+The C functions and nested loops
