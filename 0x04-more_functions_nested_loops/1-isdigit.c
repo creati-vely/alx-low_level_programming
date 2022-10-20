c08d6d100886b25a98d5dbe7bffd0e922c3309cc
creati-vely <chivunaw@gmail.com>
2022-10-20 03:43:08 -0700

    is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - code checks for a digit
+ *
+ * @c: The number to checked from 0 through 9
+ *
+ * Return: 1 if c is a digit 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48; c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
