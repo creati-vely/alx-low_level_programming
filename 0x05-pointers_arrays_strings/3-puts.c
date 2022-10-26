d55ac23207e994cb9bba481804ea24d6d950056b
creati-vely <chivunaw@gmail.com>
2022-10-26 13:55:20 -0700

    i do not fear computer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _puts - prints the string followed by a new line.
+ * @str: puts out the string stored in str.
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+
+	while (*str != '\0')
+	{
+		i++;
+	}
+	return (str);
+}
