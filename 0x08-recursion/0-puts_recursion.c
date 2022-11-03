fd9691f0e86669be2ed7b912c3620b78c8a5feb0
creati-vely <chivunaw@gmail.com>
2022-11-02 17:53:09 -0700

    tas 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,17 @@
 /**
  * _puts_recursion - prints a string using recursion
  * @s: stores the string to pu out
- * Return: return type is 0
+ * Return: return type is 1 else 0
  */
 
 void _puts_recursion(char *s)
 {
-	if (s <= 0)
-		return (0);
-	printf("%s", s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar (*s);
+	_puts_recursion(s + 1);
 }
